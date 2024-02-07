a95a57baa6c530f7dce7582916fbd4af4a6ae905
Nurbol <nurbol23072002@gmail.com>
2024-02-07 17:58:22 +0600

    First commit

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,19 +7,98 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestoreInternal
+import IQKeyboardManagerSwift
 
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    let db = Firestore.firestore()
     
+    var messages: [Message] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = K.appName
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        tableView.dataSource = self
+        navigationItem.hidesBackButton = true
+        loadMessages()
+    }
+    
+    func loadMessages(){
+        db.collection(K.FStore.collectionName).order(by: K.FStore.dateField).addSnapshotListener( { (QuerySnapshot, error) in
+            self.messages = []
+            if let e = error{
+                print("issue retrieving data")
+            }else{
+                if let snapshotDocuments = QuerySnapshot?.documents{
+                    for doc in snapshotDocuments{
+                        let data = doc.data()
+                        if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+                            self.messages.append(Message(sender: messageSender, body: messageBody))
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                                let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: false)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+      )
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email{
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField:messageSender, K.FStore.bodyField: messageBody, K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
+                if let e = error{
+                    print("issue with saving data to firebase")
+                }else{
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text = ""
+                    }
+                    print("successfully saved data ")
+                }
+            }
+        }
     }
     
+    @IBAction func logOutButtonPressed(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+    }
+    
+}
 
+extension ChatViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath   ) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        
+        if messages[indexPath.row].sender == Auth.auth().currentUser?.email{
+            cell.rightImageView.isHidden = false
+            cell.leftImageView.isHidden = true
+            cell.MessageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        }else{
+            cell.rightImageView.isHidden = true
+            cell.leftImageView.isHidden = false
+            cell.MessageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        }
+        
+        return cell
+    }
 }
---
+++ Flash Chat iOS13/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  Flash Chat iOS13
+//
+//  Created by Нурбол Мухаметжан on 27.11.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    let sender: String
+    let body: String
+}
